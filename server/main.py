7b927f96aee83aadb9c87b25b6e66eed36c04c1d
Manav Doshi <200100094@iitb.ac.in>
2021-07-27 21:31:40 +0530

    Adding both model predictions

--- server/main.py
+++ server/main.py
@@ -26,7 +26,7 @@ class Model:
         bucketname: Name of the S3 bucket
         """
         # Load the deployed model from Amazon S3
-        self.model = pycr.load_model(modelname, platform = 'aws', authentication = { 'bucket' : bucketname })
+        self.model = pycl.load_model(modelname, platform = 'aws', authentication = { 'bucket' : bucketname })
     
     def predict(self, data):
         """
@@ -41,7 +41,7 @@ class Model:
 model_rf = Model("rf_deployed", "mlops200100094")
 model_ada = Model("ada_deployed", "mlops200100094")
 # Create the POST endpoint with path '/predict'
-@app.post("/predict")
+@app.post("/rf/predict")
 # To understand how to handle file uploads in FastAPI, visit the documentation here
 async def create_upload_file(file: UploadFile = File(...)):
     # Handle the file only if it is a CSV
@@ -55,7 +55,7 @@ async def create_upload_file(file: UploadFile = File(...)):
 				
         # Return a JSON object containing the model predictions on the data
         return {
-            "Labels": model.predict(data)
+            "Labels": model_rf.predict(data)
         }
     
     else:
@@ -66,4 +66,29 @@ async def create_upload_file(file: UploadFile = File(...)):
 if os.getenv("AWS_ACCESS_KEY_ID") == None or os.getenv("AWS_SECRET_ACCESS_KEY") == None:
     print("AWS Credentials missing. Please set required environment variables.")
     exit(1)
-```
+
+@app.post("/ada/predict")
+# To understand how to handle file uploads in FastAPI, visit the documentation here
+async def create_upload_file(file: UploadFile = File(...)):
+    # Handle the file only if it is a CSV
+    if file.filename.endswith(".csv"):
+        
+        # Create a temporary file with the same name as the uploaded CSV file so that the data can be loaded into a pandas Dataframe
+        with open(file.filename, "wb")as f:
+            f.write(file.file.read())
+        data = pd.read_csv(file.filename)
+        os.remove(file.filename)
+				
+        # Return a JSON object containing the model predictions on the data
+        return {
+            "Labels": model_rf.predict(data)
+        }
+    
+    else:
+        # Raise a HTTP 400 Exception, indicating Bad Request (you can learn more about HTTP response status codes here)
+        raise HTTPException(status_code=400, detail="Invalid file format. Only CSV Files accepted.")
+
+# Check if the necessary environment variables for AWS access are available. If not, exit the program
+if os.getenv("AWS_ACCESS_KEY_ID") == None or os.getenv("AWS_SECRET_ACCESS_KEY") == None:
+    print("AWS Credentials missing. Please set required environment variables.")
+    exit(1)
